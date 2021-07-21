7fa85c55e0e72c82beb68466a6d7a7b038258a1d
gsachin121998 <gsachin121998@gmail.com>
2021-07-21 17:57:56 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,84 @@
+import asyncio
+import io
+import glob
+import os
+import sys
+import time
+import uuid
+import requests
+from urllib.parse import urlparse
+from io import BytesIO
+# To install this module, run:
+# python -m pip install Pillow
+from PIL import Image, ImageDraw
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person
+
+
+# This key will serve all examples in this document.
+KEY = "d27f0302be8245b3b6c5e6c0dca3626b"
+
+# This endpoint will be used in all examples in this quickstart.
+ENDPOINT = "https://abcfaceapi.cognitiveservices.azure.com/"
+
+
+# Create an authenticated FaceClient.
+face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
+
+
+# Detect a face in an image that contains a single face
+single_face_image_url = 'https://www.biography.com/.image/t_share/MTQ1MzAyNzYzOTgxNTE0NTEz/john-f-kennedy---mini-biography.jpg'
+single_image_name = os.path.basename(single_face_image_url)
+# We use detection model 3 to get better performance.
+detected_faces = face_client.face.detect_with_url(url=single_face_image_url, detection_model='detection_03')
+if not detected_faces:
+    raise Exception('No face detected from image {}'.format(single_image_name))
+
+# Display the detected face ID in the first single-face image.
+# Face IDs are used for comparison to faces (their IDs) detected in other images.
+print('Detected face ID from', single_image_name, ':')
+for face in detected_faces: print (face.face_id)
+print()
+
+# Save this ID for use in Find Similar
+first_image_face_ID = detected_faces[0].face_id
+
+
+
+# Detect a face in an image that contains a single face
+single_face_image_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
+single_image_name = os.path.basename(single_face_image_url)
+# We use detection model 3 to get better performance.
+detected_faces = face_client.face.detect_with_url(url=single_face_image_url, detection_model='detection_03')
+if not detected_faces:
+    raise Exception('No face detected from image {}'.format(single_image_name))
+
+# Convert width height to a point in a rectangle
+def getRectangle(faceDictionary):
+    rect = faceDictionary.face_rectangle
+    left = rect.left
+    top = rect.top
+    right = left + rect.width
+    bottom = top + rect.height
+    
+    return ((left, top), (right, bottom))
+
+def drawFaceRectangles() :
+# Download the image from the url
+    response = requests.get(single_face_image_url)
+    img = Image.open(BytesIO(response.content))
+
+# For each face returned use the face rectangle and draw a red box.
+    print('Drawing rectangle around face... see popup for results.')
+    draw = ImageDraw.Draw(img)
+    for face in detected_faces:
+        draw.rectangle(getRectangle(face), outline='red')
+
+# Display the image in the default image browser.
+
+    img.show()	
+
+
+# Uncomment this to show the face rectangles.
+#    drawFaceRectangles()
